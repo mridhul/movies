d7e026cd30b8356e4359b9bff14834fabd119e0f
mridhul <mridhul@devopsifly.com>
2018-09-10 19:27:25 +0530

    Update ecr.sh

--- ecs/scripts/ecr.sh
+++ ecs/scripts/ecr.sh
@@ -5,9 +5,9 @@
 
 set -e
 
-ECS_REGION="us-west-2"
-NAMESPACE="microservicemovies"
-IMAGE_BASE="microservicemovies"
+ECS_REGION="us-east-2"
+NAMESPACE="movies"
+IMAGE_BASE="core"
 ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
 SHORT_GIT_HASH=$(echo $CIRCLE_SHA1 | cut -c -7)
 TAG=$SHORT_GIT_HASH
@@ -27,19 +27,9 @@ tag_and_push_images() {
   echo "Tagging and pushing images..."
   $(aws ecr get-login --region "${ECS_REGION}")
   # tag
-  docker tag ${IMAGE_BASE}_users-db-review ${ECR_URI}/${NAMESPACE}/users-db-review:${TAG}
-  docker tag ${IMAGE_BASE}_movies-db-review ${ECR_URI}/${NAMESPACE}/movies-db-review:${TAG}
-  docker tag ${IMAGE_BASE}_users-service-review ${ECR_URI}/${NAMESPACE}/users-service-review:${TAG}
-  docker tag ${IMAGE_BASE}_movies-service-review ${ECR_URI}/${NAMESPACE}/movies-service-review:${TAG}
-  docker tag ${IMAGE_BASE}_web-service-review ${ECR_URI}/${NAMESPACE}/web-service-review:${TAG}
-  docker tag ${IMAGE_BASE}_swagger-review ${ECR_URI}/${NAMESPACE}/swagger-review:${TAG}
+  docker tag ${NAMESPACE}/${IMAGE_BASE} ${ECR_URI}/${NAMESPACE}/${IMAGE_BASE}:${TAG}
   # push
-  docker push ${ECR_URI}/${NAMESPACE}/users-db-review:${TAG}
-  docker push ${ECR_URI}/${NAMESPACE}/movies-db-review:${TAG}
-  docker push ${ECR_URI}/${NAMESPACE}/users-service-review:${TAG}
-  docker push ${ECR_URI}/${NAMESPACE}/movies-service-review:${TAG}
-  docker push ${ECR_URI}/${NAMESPACE}/web-service-review:${TAG}
-  docker push ${ECR_URI}/${NAMESPACE}/swagger-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/${IMAGE_BASE}:${TAG}
   echo "Images tagged and pushed!"
 }
 
