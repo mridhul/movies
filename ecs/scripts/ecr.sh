de2159ba192d5530965b82b15f29b5b99ef4439e
mridhul <mridhul@devopsifly.com>
2018-09-10 20:23:05 +0530

    Update ecr.sh

--- ecs/scripts/ecr.sh
+++ ecs/scripts/ecr.sh
@@ -6,8 +6,8 @@
 set -e
 
 ECS_REGION="us-east-2"
-NAMESPACE="microservicemovies"
-IMAGE_BASE="movies"
+NAMESPACE="movies"
+IMAGE_BASE="core"
 ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
 SHORT_GIT_HASH=$(echo $CIRCLE_SHA1 | cut -c -7)
 TAG=$SHORT_GIT_HASH
@@ -27,9 +27,9 @@ tag_and_push_images() {
   echo "Tagging and pushing images..."
   $(aws ecr get-login --region "${ECS_REGION}")
   # tag
-  docker tag ${NAMESPACE}_${IMAGE_BASE} ${ECR_URI}/${NAMESPACE}_${IMAGE_BASE}:${TAG}
+  docker tag ${NAMESPACE}/${IMAGE_BASE} ${ECR_URI}/${NAMESPACE}/${IMAGE_BASE}:${TAG}
   # push
-  docker push ${ECR_URI}/${NAMESPACE}_${IMAGE_BASE}:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/${IMAGE_BASE}:${TAG}
   echo "Images tagged and pushed!"
 }
 
